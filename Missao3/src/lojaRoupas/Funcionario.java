60e75a20546233e664d27aee7e7c2a3fbb8d91fc
Usuario <Usuario@DESKTOP-2LTK87P>
2021-09-07 16:23:13 -0300

    Commit com class funcionario

---
+++ Missao3/src/lojaRoupas/Funcionario.java
@@ -0,0 +1,13 @@
+package lojaRoupas;
+
+public class Funcionario {
+	private double identificador;
+	
+	public double getIdentificador() {
+		return identificador;
+	}
+	
+	public void setIdentificador(double identificador) {
+		this.identificador=identificador;
+	}
+}
