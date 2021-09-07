04eda90c23f85c6b4e9dbe02f029ca99ea3c981a
Usuario <Usuario@DESKTOP-2LTK87P>
2021-09-07 17:20:02 -0300

    Commit com class cartão

---
+++ Missao3/src/lojaRoupas/Cartao.java
@@ -0,0 +1,50 @@
+
+package lojaRoupas;
+public class Cartao {
+
+	private double numeroCartao;
+	private int codigoseg;
+	private String nomeCartao;
+	private String dataVenc;
+	private Cliente cliente;
+	
+	public double getNumeroCartao() {
+		return numeroCartao;
+	}
+	
+	public void setNumeroCartao(double numeroCartao) {
+		this.numeroCartao=numeroCartao;
+	}
+	
+	public int getCodigoseg() {
+		return codigoseg;
+	}
+	
+	public void setCodigoseg(int codigoseg) {
+		this.codigoseg=codigoseg;
+	}
+	
+	public String getNomeCartao() {
+		return nomeCartao;
+	}
+	
+	public void setNomeCartao(String nomeCartao) {
+		this.nomeCartao=nomeCartao;
+	}
+	
+	public String getDataVenc() {
+		return dataVenc;
+	}
+	
+	public void setDataVenc(String dataVenc) {
+		this.dataVenc=dataVenc;
+	}
+	
+	public Cliente getCliente() {
+		return cliente;
+	}
+	
+	public void setCliente(Cliente cliente) {
+		 this.cliente=cliente;
+	}
+}
