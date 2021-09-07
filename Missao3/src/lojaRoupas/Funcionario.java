081dece320a433c25ed3925b2b7d6583b204e44e
Usuario <Usuario@DESKTOP-2LTK87P>
2021-09-07 17:13:38 -0300

    Commit com class Funcionário

--- Missao3/src/lojaRoupas/Funcionario.java
+++ Missao3/src/lojaRoupas/Funcionario.java
@@ -1,13 +1,52 @@
 package lojaRoupas;
 
-public class Funcionario {
+public class Funcionario extends Pessoa {
 	private double identificador;
 	
+	public Funcionario(String n,double i,String s){
+		nome=n;
+		identificador=i;
+		senha=s;
+		
+	}
+	
+	public String toString() {
+		return "Nome do funcionário: " + nome + ", ID do funcionário" + identificador;
+	}
 	public double getIdentificador() {
 		return identificador;
 	}
 	
 	public void setIdentificador(double identificador) {
 		this.identificador=identificador;
+	}
+	public  String getNome(){
+		return nome;
+		
+	}
+	
+	public void   setNome(String nome) {
+		this.nome=nome;
+		
+	}
+	
+	public Endereço getEndereco(){
+		return endereco;
+		
+	}
+	
+	public void   setEndereço(Endereço endereco) {
+		this.endereco=endereco;
+		
+	}
+	
+	public  String getSenha(){
+		return senha;
+		
+	}
+	
+	public void   setSenha(String senha) {
+		this.senha=senha;
+		
 	}
 }
